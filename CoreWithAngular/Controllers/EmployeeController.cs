582409a83ee2ea897b45ee6731ba533ef6fe5f96
Naime Ahmed <naime1947@gmail.com>
2019-11-12 18:55:12 +0600

    Api created

---
+++ CoreWithAngular/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWithAngular.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreWithAngular.Controllers
+{
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        EmployeeManager employeeManager = new EmployeeManager();
+
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<TblEmployee> Index()
+        {
+            return employeeManager.GetAllEmployees();
+        }
+
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public int Create([FromBody] TblEmployee employee)
+        {
+            return employeeManager.AddEmployee(employee);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public TblEmployee Details(int id)
+        {
+            return employeeManager.GetEmployeeData(id);
+        }
+
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit([FromBody]TblEmployee employee)
+        {
+            return employeeManager.UpdateEmployee(employee);
+        }
+
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id)
+        {
+            return employeeManager.DeleteEmployee(id);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/GetCityList")]
+        public IEnumerable<TblCities> Details()
+        {
+            return employeeManager.GetCities();
+        }
+    }
+}
